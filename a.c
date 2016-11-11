37a89d97ca9510b6500be62796c39c5e7ecadb78
wdnst <wdnst7@gmail.com>
2016-11-11 22:21:55 +0900

    second dommit

---
+++ a.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int i=0;
+	int j=0;
+	while(1){
+		if(i==20){
+			j++;
+			if(j>10){
+				break;
+			}
+			
+		}
+		i++;
+		i%=30;
+	}
+	printf("i:%d,j:%d\r\n",i,j);
+}

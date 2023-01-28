f9cf3187e3b8434e898fdcc2779debb7932485e2
wdnst <wdnst7@gmail.com>
2023-01-28 23:48:55 +0900

    タブをスペースに置換

--- a.c
+++ a.c
@@ -6,18 +6,18 @@
 
 void main(void)
 {
-	int i=0;
-	int j=0;
-	while(1){
-		if(i==20){
-			j++;
-			if(j>10){
-				break;
-			}
-			
-		}
-		i++;
-		i%=30;
-	}
-	printf("i:%d,j:%d\r\n",i,j);
+    int i=0;
+    int j=0;
+    while(1){
+        if(i==20){
+            j++;
+            if(j>10){
+                break;
+            }
+            
+        }
+        i++;
+        i%=30;
+    }
+    printf("i:%d,j:%d\r\n",i,j);
 }
